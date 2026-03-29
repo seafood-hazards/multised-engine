a63a3d9b4c25df3b8752835390e81f9830e591fb
Takaya Saito <takaya.saito@hi.no>
2026-03-30 01:29:16 +0200

    Add category to the export data set

--- R/vannmiljo/07_create_data_frame.R
+++ R/vannmiljo/07_create_data_frame.R
@@ -12,12 +12,15 @@ df_site <- dbReadTable(con, "site") |> as_tibble()
 df_sample_method <- dbReadTable(con, "sample_method") |> as_tibble()
 df_analysis_method <- dbReadTable(con, "analysis_method") |> as_tibble()
 df_sample <- dbReadTable(con, "sample") |> as_tibble()
-df_parameter <- dbReadTable(con, "parameter") |> as_tibble()
+df_parameter <- dbReadTable(con, "parameter") |> as_tibble() %>%
+  mutate(category = case_when(
+    param_id %in% c("TOC", "TOC63", "TC", "TIC", "TS")  ~ "tcs",
+    param_id %in% c("GSMF2_63", "GSMF_2000", "GSMF125_250", "GSMF1000_2000", "GSMF250_500", "GSMF63_125", "GSMF500_1000", "GSMF_63", "GSMF2", "FINS")  ~ "p",
+    param_id %in% c("CO", "CU", "I", "MN", "MO", "SE", "ZN")  ~ "efsa",
+    TRUE ~ "other"
+  ))
 df_sediment <- dbReadTable(con, "sediment") |> as_tibble()
 df_lld <- dbReadTable(con, "lld") |> as_tibble()
-
-# Disconnect
-dbDisconnect(con)
 
 df_vannmiljo_sediment <- df_sediment %>%
   inner_join(df_sample %>%
@@ -37,9 +40,12 @@ df_vannmiljo_sediment <- df_sediment %>%
             by = c("sample_id", "param_id", "sediment_no")) %>%
   dplyr::select(activity_id, activity_name, site_code, site_name,
                 lon, lat, dist_to_coast, country, country_code, municipality, sea_name,
-                sample_time, upper_depth, lower_depth, sample_no, n_values,
-                param_id, param_name, method, analysis, value, unit, operator, lod, loq)
+                sample_id, sample_time, sediment_no, upper_depth, lower_depth, sample_no, n_values,
+                param_id, param_name, method, analysis, value, unit, operator, lod, loq,
+                category)
 
+# Disconnect
+dbDisconnect(con)
 
 write_tsv(df_vannmiljo_sediment, "./data/pilot_vannmiljo.tsv.gz")
 
@@ -50,3 +56,4 @@ activity_table <- df_vannmiljo_sediment %>% count(activity_id, activity_name, pa
                 Cobalt, Copper, Manganese, Molybdenum, Zinc, Selenium)
 
 write_tsv(activity_table, "./data/vannmiljo_activities.tsv.gz")
+
