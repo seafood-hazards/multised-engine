d8377dc6fd0fa7113c9d1ebfd290038d24d52a1d
Takaya Saito <takaya.saito@hi.no>
2026-04-22 22:43:20 +0200

    Repalce icorrect grain unit descriptions

--- R/mareano/01_extract_meta_information.R
+++ R/mareano/01_extract_meta_information.R
@@ -144,7 +144,8 @@ correct_df_element_info <- function(df) {
                  )) %>%
     dplyr::select(parameter, element, method1, method2, institute) %>%
     separate(parameter, c("symbol"), remove = FALSE, extra = "drop") %>%
-    mutate(element = ifelse(parameter == "Cd_p", "Cadmium", element)) %>%
+    mutate(element = ifelse(parameter == "Cd_p", "Cadmium",
+                            str_replace(element, "\\?m", "µm"))) %>%
     dplyr::bind_rows(
       tibble::tibble(
         parameter = "S_p",
